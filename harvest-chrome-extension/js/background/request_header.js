8cd494c294b15697fc7c23b8928018d20ea1d8da
Anton Konovalov <extempl@gmail.com>
2016-09-07 09:47:09 +0300

    Refactor to be standalone expansion of the original extension.

--- harvest-chrome-extension/js/background/request_header.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  (function() {
-    var _base;
-    (_base = $.ajaxSettings).headers || (_base.headers = {});
-    return $.ajaxSettings.headers["X-HarvestChromeExt"] = chrome.app.getDetails().version;
-  })();
-
-}).call(this);
